68d537e5a07c912ee98f82fe0d0af829a6c7a9e5
zkakskdk <wltjd488946@naver.com>
2025-11-03 20:35:27 +0900

    1

---
+++ IOT/main6_1_2.py
@@ -0,0 +1,5 @@
+import random
+
+n = (random.randrange(1, 100))
+
+print("1~100 사이의 숫자를 입력하세요",(n))
---
+++ test11.03/client.py
@@ -0,0 +1,24 @@
+# client.py
+import socket, sys
+from protocol import pack_frame, recv_frame
+
+class EchoClient:
+    def __init__(self, host="127.0.0.1", port=25000):
+        self.host = host
+        self.port = port
+
+    def run(self, n: int, msg: str):
+        with socket.create_connection((self.host, self.port)) as s:
+            s.sendall(pack_frame(n, msg))
+            for i in range(1, n+1):
+                n_resp, msg_resp = recv_frame(s)
+                print(f"[{i}/{n_resp}] {msg_resp}")
+
+if __name__ == "__main__":
+    # 예: Windows에서 클라이언트
+    # 사용: python client.py 127.0.0.1 25000 3 "hello"
+    host = sys.argv[1] if len(sys.argv) > 1 else "127.0.0.1"
+    port = int(sys.argv[2]) if len(sys.argv) > 2 else 25000
+    n    = int(sys.argv[3]) if len(sys.argv) > 3 else 3
+    msg  = sys.argv[4] if len(sys.argv) > 4 else "hello"
+    EchoClient(host, port).run(n, msg)
